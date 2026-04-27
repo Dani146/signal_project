5a34b914f563cc0a63801785c0c828d4e64195e1
Matiiss <83066658+Matiiss@users.noreply.github.com>
2026-04-27 18:46:32 +0200

    Add javadoc comments for PatientDataGenerator (interface)

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,16 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * Describes the interface for patient data simulators.
+ */
 public interface PatientDataGenerator {
+    /**
+     * Generates patient data for a particular patient (based on {@code patientId}) and outputs it
+     * to the given {@code outputStrategy}.
+     * 
+     * @param patientId id of the patient for whom to generate the data
+     * @param outputStrategy output strategy to use for output
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
