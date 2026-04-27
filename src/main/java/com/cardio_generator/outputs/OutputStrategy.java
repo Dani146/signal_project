a2d765824559c2d2fcdb0f9b85379a76f9eac98c
Matiiss <83066658+Matiiss@users.noreply.github.com>
2026-04-27 18:51:46 +0200

    Add javadoc comments for OutputStrategy (interface)

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,18 @@
 package com.cardio_generator.outputs;
 
+/**
+ * Describes the interface for output strategies. For example, output could be printed to the
+ * terminal, or it could be sent over network to a client.
+ */
 public interface OutputStrategy {
+    /**
+     * Outputs the given data to where the strategy implementation defines.
+     * 
+     * @param patientId id of the patient
+     * @param timestamp the timestamp of this output (typically, the current time at the time of
+     *        calling this method)
+     * @param label label for the data (e.g., ECG, Cholesterol)
+     * @param data the data described by the label
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
